16304a32cc471dacb409d111856fe66ca66adcd5
BhavinThakkar18 <bhavin.majethia18@gmail.com>
2022-01-19 15:07:28 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Stack<String> stack=new Stack<>();
+	System.out.println("START main");
+	stack.push("main");
+	System.out.println("START builder");
+	stack.push("builder");
+	System.out.println("START external-servcice");
+	stack.push("external-service");
+	System.out.println("END "+stack.pop());
+	System.out.println("START parse-external-data");
+	stack.push("parse-external-data");
+	System.out.println("END "+stack.pop());
+	System.out.println("END "+stack.pop());
+	System.out.println("END "+stack.pop());
+
+    }
+}
